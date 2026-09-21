4fb7f04e9ac2ad0c818e0c17d9c4f721004528ad
EzekielAs <asomaniezekiel8@gmail.com>
2026-09-21 10:45:39 +0000

    fix cors

--- SERVER/index.js
+++ SERVER/index.js
@@ -10,7 +10,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const db = sqlite3('wonjuga.db');
 
-app.use(cors());
+app.use(cors({origin: "*", methods: ["GET","POST","PUT","DELETE"], allowedHeaders: ["Content-Type","Authorization"]}));
 app.use(express.json());
 app.use(helmet());
 app.use(rateLimit({windowMs:15*60*1000, max:200}));
